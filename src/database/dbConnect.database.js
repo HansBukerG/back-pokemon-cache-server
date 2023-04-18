787d11bfdb2f28d783f149a8e6bc83be477f7773
Hans Buker <hansbuker1993@gmail.com>
2023-04-18 17:12:00 -0400

    feat: database connection

--- src/database/dbConnect.database.js
+++ src/database/dbConnect.database.js
@@ -2,12 +2,44 @@ import { MongoClient } from 'mongodb';
 
 const mongoConnectionScheme = process.env.MONGO_CONNECTION_SCHEME;
 const mongoHost = process.env.MONGO_HOST;
+const mongoPort = process.env.MONGO_PORT;
 const mongoUser = process.env.MONGO_USER;
 const mongoPassword = process.env.MONGO_PASSWORD;
 const mongoDatabase = process.env.MONGO_DATABASE;
+const mongoPokeApiCollection = process.env.MONGO_POKEAPI_COLLECTION;
+const mongoDigiApiCollection = process.env.MONGO_DIGIAPI_COLLECTION;
 
-const mongoUri = `${mongoConnectionScheme}://${mongoUser}:${mongoPassword}@${mongoHost}/${mongoDatabase}`;
+const mongoUri = `${mongoConnectionScheme}://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}`;
 
-const mongoClient = new MongoClient(mongoUri);
+// export const mongoClient = new MongoClient(mongoUri);
+export const mongoClient = new MongoClient(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-export { mongoClient };
+export const getDbConnection = async () =>
+{
+  if(mongoClient == undefined){
+    try
+    {
+      console.log(`Setting up a new connection to: ${mongoUri}`);
+      await mongoClient.connect();
+    } catch (error)
+    {
+      console.error('Error connecting to MongoDB', error);
+      throw error;
+    }
+  }
+  const database = mongoClient.db(mongoDatabase);
+  return database;
+};
+
+export const getCollection = async (name) => {
+  const database = await getDbConnection();
+  return database.collection(name);
+};
+
+export const getPokeApiCollection = async () => {
+  return await getCollection(mongoPokeApiCollection);
+};
+
+export const getDigiApiCollection = async () => {
+  return await getCollection(mongoDigiApiCollection);
+};
--- src/database/dbConnect.database.test.js
+++ src/database/dbConnect.database.test.js
@@ -1,17 +1,30 @@
-import  {dbClient} from './dbConnect.database';
+import { mongoClient, getDbConnection, getCollection ,getPokeApiCollection, getDigiApiCollection } from './dbConnect.database.js';
 
 describe('Should get a connection from the database', () =>{
-    test('Should get a connection to MongoDB', async () => {
 
-        try {
-            await dbClient.connect();
-            // Establish and verify connection
-            const result = await dbClient.db("promotions").command({ ping: 1 });
-            console.log(`result: ${result}`);
-        } catch (error) {
-            console.log(error);
-        } finally {
-            await dbClient.close(); 
-        }
-    });
+  test('Should get a response from pokeApi Collection', async()=>{
+    try
+    {
+      const dbConnection = await getDbConnection();
+      console.log('Ping to database was successful');
+
+      const pokeApiCollection = getPokeApiCollection();
+      const digiApiCollection = getDigiApiCollection();
+
+      const pokeApiDocsCount = (await pokeApiCollection).countDocuments();
+      const digiApiDocsCount = (await digiApiCollection).countDocuments();
+
+      console.log(`pokeApi collection has ${(await pokeApiDocsCount).valueOf()} documents`);
+      console.log(`digiApi collection has ${(await digiApiDocsCount).valueOf()} documents`);
+
+    } catch (error)
+    {
+      console.log(`Error executing test: ${error}`);
+      fail();
+    }finally{
+      await mongoClient.close();
+    }
+
+  });
+
 });
